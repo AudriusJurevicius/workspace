c2f23af3642e849615507cf5c4170580db5faa48
Audrius Jurevicius <Audrius1992@gmail.com>
2020-01-08 13:55:35 +0000

    dabaigsiu rytdiena

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const http = require('http')
 const port = 1234
 
 const fs = require('fs')
+const tickets = require('./tickets.json')
+
+const express = require('express')
 
 const server = http.createServer((req, res) => {
     console.log(`request was made: ${req.url}`)
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    fs.readFile('tickets.json', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('issue-ticket.html', (err, data) => {
         if (err) {
             res.writeHead(404)
             res.write('File not found!')
-        } else {
-            res.write(data)
         }
-    res.end()
+    res.end(data)
     })
 })
 
