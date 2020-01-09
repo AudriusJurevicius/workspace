8528eea92b8cf6f217cbcfbc8e2ede5142c85f72
Audrius Jurevicius <Audrius1992@gmail.com>
2020-01-09 10:15:15 +0000

    pradetas mokytis express

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const http = require('http')
-const port = 1234
-
 const fs = require('fs')
 const tickets = require('./tickets.json')
 
 const express = require('express')
 
-const server = http.createServer((req, res) => {
-    console.log(`request was made: ${req.url}`)
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('issue-ticket.html', (err, data) => {
-        if (err) {
-            res.writeHead(404)
-            res.write('File not found!')
-        }
-    res.end(data)
-    })
-})
+const app = express()
 
-server.listen(port, (error) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log('Server is running!')
-    }
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} port!`)
 })
